b3f37e06f892d0cfe6688586a461a112e46fc80d
LuisSiccardi <84135938+LuisSiccardi@users.noreply.github.com>
2022-10-20 10:26:16 -0300

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,8 @@
+print("suma dos numeros enteros") #funcion para mostrar en pantalla
+
+numero1 = int(input("ingrese el primer numero: "))
+numero2 = int(input("ingrese el segundo numero: "))
+
+print(numero1+numero2)
+
+print("fin")
