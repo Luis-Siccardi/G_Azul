ecc23d45620d0c77b5339f4e36d441291d9472dd
LuisSiccardi <84135938+LuisSiccardi@users.noreply.github.com>
2022-10-20 10:27:28 -0300

    Delete suma.py

--- suma.py
+++
@@ -1,8 +0,0 @@
-print("suma dos numeros enteros") #funcion para mostrar en pantalla
-
-numero1 = int(input("ingrese el primer numero: "))
-numero2 = int(input("ingrese el segundo numero: "))
-
-print(numero1+numero2)
-
-print("fin")
